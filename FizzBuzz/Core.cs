ea4fb1db11d8e6a0b670992c2d99759667b335c8
Ars_Mond <ars123.www@gmail.com>
2022-09-08 00:34:53 +0500

    Release

---
+++ FizzBuzz/Core.cs
@@ -0,0 +1,59 @@
+﻿namespace FizzBuzz;
+
+public class Core
+{
+    public string[] Data => _data;
+
+
+    private string[] _data;
+    private int _count;
+    private readonly string _fizz = "Fizz";
+    private readonly string _buzz = "Buzz";
+    
+    
+    public Core(int count)
+    {
+        _count = count;
+        _data = new string[count];
+    }
+
+
+    public string[] Generate()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            _data[i] = i.ToString();
+            if(i%3 == 0) _data[i] = _fizz;
+            else if(i%5 == 0) _data[i] = _buzz;
+        }
+
+        return _data;
+    }
+
+    public override string ToString()
+    {
+        return String.Join('\n', _data);
+    }
+
+    public string ToStringFast()
+    {
+        string a = "";
+        for (int i = 0; i < _count; i++)
+        {
+            a += _data[i] + '\n';
+        }
+        return a;
+    }
+    
+    public void ToStringSlow()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            Console.WriteLine(_data[i]);
+        }
+    }
+    
+    
+    
+
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FizzBuzz
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write count num: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Stopwatch alltime = new Stopwatch();
+            Stopwatch runtime = new Stopwatch();
+            Stopwatch outtime = new Stopwatch();
+
+
+            alltime.Start();
+            Console.WriteLine("Start generation!");
+            Core core = new Core(count);
+            
+            runtime.Start();
+
+            core.Generate();
+            
+            runtime.Stop();
+            
+            outtime.Start();
+            
+            Console.WriteLine(core.ToString());
+            //core.ToStringSlow();
+                
+            outtime.Stop();
+            alltime.Start();
+
+            string alltimeStr = TimeFormat(alltime.Elapsed);
+            string runtimeStr = TimeFormat(runtime.Elapsed);
+            string outtimeStr = TimeFormat(outtime.Elapsed);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Runtime >> " + runtimeStr);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Outtime >> " + outtimeStr);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("______________________________");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Runtime >> " + alltimeStr);
+            Console.WriteLine();
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            long frequency = Stopwatch.Frequency;
+            Console.WriteLine("  Timer frequency in ticks per second = {0}", frequency);
+            
+            long nanosecPerTick = (1000L*1000L*1000L) / frequency;
+            Console.WriteLine("  Timer is accurate within {0} nanoseconds", nanosecPerTick);
+
+            long nanoseconds = nanosecPerTick * runtime.ElapsedTicks;
+            Console.WriteLine("  Runtime nanoseconds >> " + nanoseconds);
+            Console.ReadLine();
+        }
+
+        static string TimeFormat(TimeSpan time)
+        {
+            return $"{time.Hours:00}:{time.Minutes:00}:{time.Seconds:00}.{time.Milliseconds:000}::tck.{time.Ticks}";
+        }
+    }
+}
