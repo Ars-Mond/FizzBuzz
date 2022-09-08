c11a31866467bb7a4c8cfce04a496dd16a494e4a
Ars_Mond <ars123.www@gmail.com>
2022-09-08 15:37:06 +0500

    Release3

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FizzBuzz
 {
@@ -35,32 +36,55 @@ namespace FizzBuzz
             string alltimeStr = TimeFormat(alltime.Elapsed);
             string runtimeStr = TimeFormat(runtime.Elapsed);
             string outtimeStr = TimeFormat(outtime.Elapsed);
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Runtime >> " + runtimeStr);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Outtime >> " + outtimeStr);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("______________________________");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Runtime >> " + alltimeStr);
-            Console.WriteLine();
+            
+            NewLine(1);
+            ColourOut("Runtime >> " + runtimeStr, ConsoleColor.Cyan);
+            ColourOut("Outtime >> " + outtimeStr, ConsoleColor.Yellow);
+            ColourOut("______________________________", ConsoleColor.White);
+            ColourOut("Runtime >> " + alltimeStr, ConsoleColor.Magenta);
+            NewLine(1);
             
             Console.ForegroundColor = ConsoleColor.White;
             long frequency = Stopwatch.Frequency;
             Console.WriteLine("  Timer frequency in ticks per second = {0}", frequency);
             
-            long nanosecPerTick = (1000L*1000L*1000L) / frequency;
-            Console.WriteLine("  Timer is accurate within {0} nanoseconds", nanosecPerTick);
+            long nanosecondsPerTick = (1000L*1000L*1000L) / frequency;
+            Console.WriteLine("  Timer is accurate within {0} nanoseconds", nanosecondsPerTick);
 
-            long nanoseconds = nanosecPerTick * runtime.ElapsedTicks;
-            Console.WriteLine("  Runtime nanoseconds >> " + nanoseconds);
-            Console.ReadLine();
+            NewLine(1);
+            long nanoseconds = nanosecondsPerTick * runtime.ElapsedTicks;
+            ColourOut("  Runtime nanoseconds >> ", ConsoleColor.White, nanoseconds.ToString(), ConsoleColor.Red);
+
+            nanoseconds = nanosecondsPerTick * outtime.ElapsedTicks;
+            ColourOut("  Outtime nanoseconds >> ", ConsoleColor.White, nanoseconds.ToString(), ConsoleColor.Red);
+            
+            NewLine(1);
+            ColourOut("Press any key to continue...", ConsoleColor.Gray);
+            Console.ReadKey();
         }
 
         static string TimeFormat(TimeSpan time)
         {
             return $"{time.Hours:00}:{time.Minutes:00}:{time.Seconds:00}.{time.Milliseconds:000}::tck.{time.Ticks}";
+        }
+
+        static void ColourOut(string value, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+        }
+        
+        static void ColourOut(string value, ConsoleColor color, string value2, ConsoleColor color2)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ForegroundColor = color2;
+            Console.WriteLine(value2);
+        }
+
+        static void NewLine(uint count)
+        {
+            for (int i = 0; i < count; i++) Console.WriteLine();
         }
     }
 }
